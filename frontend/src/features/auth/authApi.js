795a3ddee6a5abebfc73dd11d4263e32c7695ead
emmieIO <markonuoha6@gmail.com>
2024-09-10 03:36:39 +0100

    fixing cors error

--- frontend/src/features/auth/authApi.js
+++ frontend/src/features/auth/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:4200/api/auth',
+        baseUrl: 'https://bus-naija.onrender.com/api/auth',
         credentials: 'include'
     }),
     endpoints: (builder) => {
